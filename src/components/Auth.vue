286067506f7e4839d256eff2582a24cac9ecac10
jamil-codemen <jamil@codemen.net>
2021-07-15 16:04:08 +0600

    Vue-Vuex-Auth
    	1:Validation with veevalidate
    	2:Used as a global plugin

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -103,55 +103,65 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab==='register'">
+          <vee-form v-show="tab==='register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
                 type="text"
+                name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage class="text-red-600" name="name"/>
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email"/>
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
                 type="number"
+                name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
               />
+              <ErrorMessage class="text-red-600" name="age"/>
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password"/>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <ErrorMessage class="text-red-600" name="confirm_password"/>
             </div>
             <!-- Country -->
             <div class="mb-3">
@@ -180,7 +190,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
@@ -194,7 +204,14 @@ export default {
   name: "Auth",
   data(){
     return{
-      tab:'login'
+      tab:'login',
+      schema:{
+        name:'required|min:3|max:20|alpha_spaces',
+        email:'required|min:3|max:30|email',
+        age:'required|min_value:18|max_value:100',
+        password:'required|min:3|max:100',
+        confirm_password:'confirmed:@password'
+      }
     };
   },
   computed: {
